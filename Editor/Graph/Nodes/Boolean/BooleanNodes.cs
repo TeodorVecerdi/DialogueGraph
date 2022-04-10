e28a384f9a702cfe09fd98451b17d9863b002c8b
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-04-10 16:16:09 +0300

    Rename boolean node titles to uppercase

--- Editor/Graph/Nodes/Boolean/BooleanNodes.cs
+++ Editor/Graph/Nodes/Boolean/BooleanNodes.cs
@@ -1,41 +1,41 @@
 namespace DialogueGraph {
-    [Title("Boolean", "Not")]
+    [Title("Boolean", "NOT")]
     public class NotBooleanNode : UnaryBooleanNode {
         protected override string Title => "Not/Negate";
         protected override BooleanOperation Operation => BooleanOperation.NOT;
     }
 
-    [Title("Boolean", "And")]
+    [Title("Boolean", "AND")]
     public class AndBooleanNode : BinaryBooleanNode {
         protected override string Title => "And";
         protected override BooleanOperation Operation => BooleanOperation.AND;
     }
 
-    [Title("Boolean", "Or")]
+    [Title("Boolean", "OR")]
     public class OrBooleanNode : BinaryBooleanNode {
         protected override string Title => "Or";
         protected override BooleanOperation Operation => BooleanOperation.OR;
     }
 
-    [Title("Boolean", "Xor")]
+    [Title("Boolean", "XOR")]
     public class XorBooleanNode : BinaryBooleanNode {
         protected override string Title => "Xor";
         protected override BooleanOperation Operation => BooleanOperation.XOR;
     }
 
-    [Title("Boolean", "Nand")]
+    [Title("Boolean", "NAND")]
     public class NandBooleanNode : BinaryBooleanNode {
         protected override string Title => "Nand";
         protected override BooleanOperation Operation => BooleanOperation.NAND;
     }
 
-    [Title("Boolean", "Nor")]
+    [Title("Boolean", "NOR")]
     public class NorBooleanNode : BinaryBooleanNode {
         protected override string Title => "Nor";
         protected override BooleanOperation Operation => BooleanOperation.NOR;
     }
 
-    [Title("Boolean", "Xnor")]
+    [Title("Boolean", "XNOR")]
     public class XnorBooleanNode : BinaryBooleanNode {
         protected override string Title => "Xnor";
         protected override BooleanOperation Operation => BooleanOperation.XNOR;
