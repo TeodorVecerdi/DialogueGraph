d90c5ebcd088bca95c12fc438c3cd4718e5f08f5
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2023-08-12 14:26:55 +0300

    Add extension methods for converting editor types to runtime types

---
+++ Editor/Extensions/EditorToRuntimeConversionExtensions.cs
@@ -0,0 +1,28 @@
+﻿#nullable enable
+
+using System;
+using DialogueGraph.Runtime;
+
+namespace DialogueGraph {
+    internal static class EditorToRuntimeConversionExtensions {
+        internal static Property ToRuntime(this AbstractProperty property)
+            => new(property.GUID, property.ReferenceName, property.DisplayName, property.Type);
+
+        internal static Edge ToRuntime(this SerializedEdge edge)
+            => new(edge.Output, edge.OutputPort, edge.Input, edge.InputPort);
+
+        internal static ConversationLine ToRuntime(this LineDataSelf line) => new() {
+            Message = line.Line,
+            Next = line.PortGuidA,
+            TriggerPort = line.PortGuidB,
+            CheckPort = Guid.Empty.ToString(),
+        };
+
+        internal static ConversationLine ToRuntime(this LineDataNpc line) => new() {
+            Message = line.Line,
+            Next = line.PortGuidA,
+            TriggerPort = line.PortGuidB,
+            CheckPort = line.PortGuidC,
+        };
+    }
+}
